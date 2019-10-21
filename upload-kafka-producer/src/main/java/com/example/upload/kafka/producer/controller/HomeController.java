d9945715e8c626b4aba7abb0768713bab16a70a4
ECD <ecofre@gmail.com>
2019-10-20 21:58:01 -0300

    Se agrega formulario para enviar mensaje a Kafka

--- upload-kafka-producer/src/main/java/com/example/upload/kafka/producer/controller/HomeController.java
+++ upload-kafka-producer/src/main/java/com/example/upload/kafka/producer/controller/HomeController.java
@@ -1,14 +1,41 @@
 package com.example.upload.kafka.producer.controller;
 
+import com.example.upload.kafka.producer.form.KafkaForm;
+import com.example.upload.kafka.producer.services.KafkaProducer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
 
+    static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+
+    @Autowired
+    private KafkaProducer kafkaProducer;
+
     @RequestMapping("/")
-    public String home() {
+    public String home(Model model) {
+        logger.debug("[UploadController][home][START]");
+
+        KafkaForm kafkaForm = new KafkaForm();
+        model.addAttribute("kafkaForm", kafkaForm);
+
+        logger.debug("[UploadController][home][END]");
         return "home";
+    }
+
+    @RequestMapping("/publish")
+    public String sendMessageToKafka(@ModelAttribute("uploadForm") KafkaForm kafkaForm) {
+        logger.info("[KafkaController][sendMessageToKafkaTopic][message: " + kafkaForm.getMessage() + "]");
+        this.kafkaProducer.sendMessage(kafkaForm.getMessage());
+
+        logger.info("[KafkaController][sendMessageToKafkaTopic][END]");
+        return "result";
     }
 
 }
---
+++ upload-kafka-producer/src/main/java/com/example/upload/kafka/producer/form/KafkaForm.java
@@ -0,0 +1,17 @@
+package com.example.upload.kafka.producer.form;
+
+public class KafkaForm {
+
+    private String message;
+
+    public KafkaForm() {
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
