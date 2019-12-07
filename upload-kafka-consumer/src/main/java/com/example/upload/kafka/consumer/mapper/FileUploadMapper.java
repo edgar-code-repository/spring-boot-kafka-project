3ca848121d5106ae99ccfe0861fe75f9ad35557f
Edgardo Cofré Durán <ecofre@gmail.com>
2019-12-06 22:41:52 -0300

    Se procesa archivo desde aplicacion consumer y se cambia el estado del registro en base de datos de processing a completed

--- upload-kafka-consumer/src/main/java/com/example/upload/kafka/consumer/mapper/FileUploadMapper.java
+++ upload-kafka-consumer/src/main/java/com/example/upload/kafka/consumer/mapper/FileUploadMapper.java
@@ -3,11 +3,15 @@ package com.example.upload.kafka.consumer.mapper;
 import com.example.upload.kafka.consumer.to.FileUpload;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 
 @Mapper
 public interface FileUploadMapper {
 
-    @Select("SELECT * FROM tbl_file_upload WHERE file_upload_id = #{fileUploadId}")
+    @Select("SELECT * FROM tbl_file_upload WHERE file_upload_id=#{fileUploadId}")
     FileUpload getFileUploaded(Integer fileUploadId);
+
+    @Update("UPDATE tbl_file_upload set state=#{state} where file_upload_id=#{fileUploadId}")
+    void update(FileUpload fileUpload);
 
 }
--- upload-kafka-consumer/src/main/java/com/example/upload/kafka/consumer/service/KafkaConsumer.java
+++ upload-kafka-consumer/src/main/java/com/example/upload/kafka/consumer/service/KafkaConsumer.java
@@ -8,6 +8,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.concurrent.TimeUnit;
+
 @Service
 public class KafkaConsumer {
 
@@ -22,14 +26,34 @@ public class KafkaConsumer {
     }
 
     @KafkaListener(id="kafka-process-file", topics="kafka-process-file")
-    public void processFileUploaded(String fileUploadId) {
+    public void processFileUploaded(String fileUploadId) throws InterruptedException {
         logger.info("[KafkaConsumer][processFileUploaded][fileUploadId: " + fileUploadId + "]");
 
         FileUpload fileUploaded = fileUploadMapper.getFileUploaded(Integer.parseInt(fileUploadId));
-        logger.info("[KafkaConsumer][processFileUploaded][fileUploaded name: " + fileUploaded.getFilename() + "]");
-        logger.info("[KafkaConsumer][processFileUploaded][The file retrieved from database should be processed]");
+        logger.info("[KafkaConsumer][processFileUploaded][fileUploaded: " + fileUploaded.toString() + "]");
 
-        
+        DateTimeFormatter myFormatObject = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+
+        logger.info("[KafkaConsumer][processFileUploaded][Date previous sleep: " + LocalDateTime.now().format(myFormatObject) + "]");
+        TimeUnit.SECONDS.sleep(15);
+        logger.info("[KafkaConsumer][processFileUploaded][Date after sleep: " + LocalDateTime.now().format(myFormatObject) + "]");
+
+        logger.info("[KafkaConsumer][processFileUploaded][Updating State]");
+        FileUpload fileUpdated = new FileUpload();
+        fileUpdated.setFileUploadId(Integer.parseInt(fileUploadId));
+        fileUpdated.setState("C");
+        fileUploadMapper.update(fileUpdated);
+
+        fileUpdated = fileUploadMapper.getFileUploaded(Integer.parseInt(fileUploadId));
+
+        if (fileUpdated != null) {
+            logger.info("[KafkaConsumer][processFileUploaded][fileUpdated: " + fileUpdated.toString() + "]");
+            logger.info("[KafkaConsumer][processFileUploaded][The file was processed]");
+        }
+        else {
+            logger.info("[KafkaConsumer][processFileUploaded][fileUpdated NULL]");
+        }
+
     }
 
 }
